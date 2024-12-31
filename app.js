892ca7ce6dfb7a44e2a3509468ecfa3a3eb5985f
Bảo <nguyenvanbaom1515@gmail.com>
2024-12-31 14:14:11 +0700

    tcc

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const { google } = require('googleapis');
 const path = require('path');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 const KEY_FILE_PATH = path.join(__dirname, 'subtle-lambda-438005-m6-9c5ab8ef5764.json');
-const SPREADSHEET_ID = '1bryAfC8RNKOVrHSwqfmPNQ1KIZjueQYEtqSTW9vQLQg';
-const RANGE = 'Sheet1!A2:Z1000';
+const SPREADSHEET_ID = '12IdVLhUNYU57tEZ6kE-DVtrZyhcAmlReG73pRUDI-WY';
+const RANGE = '2024!B9:AG1000';
 
 const sheets = google.sheets('v4');
 
@@ -27,8 +28,7 @@ async function getSheetData() {
 
 async function findValuesByName(name) {
   const data = await getSheetData();
-
-  const rows = data.filter((row) => row[0]?.toLowerCase() === name.toLowerCase());
+  const rows = data.filter((row) => row[2]?.toLowerCase() === name.toLowerCase());
   return rows.length > 0 ? rows[0] : null;
 }
 
@@ -38,9 +38,27 @@ const port = 3001;
 
 // Sử dụng CORS middleware để cho phép yêu cầu từ các domain khác
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/find-value', async (req, res) => {
-  const name = req.query.name;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 phút
+  max: 10, // Tối đa 3 request mỗi IP
+  standardHeaders: true, // Hiển thị thông tin giới hạn trong headers
+  legacyHeaders: false, // Tắt X-RateLimit-* headers (cũ)
+  message: { error: 'Bạn đã gửi quá nhiều yêu cầu, vui lòng thử lại sau 15 phút.' },
+  statusCode: 429, // Mã trạng thái cho quá nhiều yêu cầu
+  handler: (req, res) => {
+    res.status(429).json({
+      status: 429,
+      error: 'Quá nhiều yêu cầu từ IP này, vui lòng thử lại sau 15 phút.',
+    });
+  },
+});
+
+app.post('/api/find-value', limiter, async (req, res) => {
+  const { name } = req.body;
+
   if (!name) {
     return res.status(400).json({ error: 'Tên không được phép rỗng.' });
   }
@@ -49,14 +67,35 @@ app.get('/api/find-value', async (req, res) => {
     const rows = await findValuesByName(name);
     console.log(rows);
 
-    if (rows) {
-      res.status(200).json({ data: rows, status: 200 });
+    if (rows && rows.length > 0) {
+      res.status(200).json({
+        data: {
+          EmployeeId: rows[0],
+          EmployeeName: rows[1],
+          position: rows[5],
+          jan: rows[17],
+          feb: rows[18],
+          mar: rows[19],
+          apr: rows[20],
+          may: rows[21],
+          jun: rows[22],
+          jul: rows[23],
+          aug: rows[24],
+          sep: rows[25],
+          oct: rows[26],
+          nov: rows[27],
+          dec: rows[28],
+          restUse: rows[29],
+          restNotUse: rows[30],
+        },
+        status: 200,
+      });
     } else {
       res.status(404).json({ error: 'Không tìm thấy tên trong bảng tính.' });
     }
   } catch (error) {
     console.error('Lỗi:', error);
-    res.status(500).json({ error: 'Lỗi khi truy xuất dữ liệu từ Google Sheets.' });
+    res.status(500).json({ error: 'Lỗi khi truy xuất dữ liệu.' });
   }
 });
 
