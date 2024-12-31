7ec782f6b0d6375d814422e27a32986f6133dc76
Bảo <nguyenvanbaom1515@gmail.com>
2024-12-31 14:17:55 +0700

    tccc

--- app.js
+++ app.js
@@ -36,18 +36,17 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-// Sử dụng CORS middleware để cho phép yêu cầu từ các domain khác
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 phút
-  max: 10, // Tối đa 3 request mỗi IP
-  standardHeaders: true, // Hiển thị thông tin giới hạn trong headers
-  legacyHeaders: false, // Tắt X-RateLimit-* headers (cũ)
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
   message: { error: 'Bạn đã gửi quá nhiều yêu cầu, vui lòng thử lại sau 15 phút.' },
-  statusCode: 429, // Mã trạng thái cho quá nhiều yêu cầu
+  statusCode: 429,
   handler: (req, res) => {
     res.status(429).json({
       status: 429,
