269a22774898b3afabdd1c49aa2e3a06d05a2d4a
devballllller <hr2@ballerheadwear.com>
2025-09-24 13:23:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+// const express = require('express');
+// const cors = require('cors');
+// const sheetRoutes = require('./routes/sheetRoutes');
+// require('dotenv').config();
+// const http = require('http'); // Tạo HTTP server
+// const WebSocketManager = require('./config/websocket/websocketConfig'); // Import WebSocket manager
+
+// const app = express();
+// const port = 3999;
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api', sheetRoutes);
+
+// // Tạo HTTP server để WebSocket có thể sử dụng chung
+// const server = http.createServer(app);
+
+// // Khởi động WebSocket Server
+// WebSocketManager.setup(server);
+// server.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+
 const express = require('express');
 const cors = require('cors');
 const sheetRoutes = require('./routes/sheetRoutes');
 require('dotenv').config();
-const http = require('http'); // Tạo HTTP server
-const WebSocketManager = require('./config/websocket/websocketConfig'); // Import WebSocket manager
 
 const app = express();
-const port = 3999;
 
 app.use(cors());
 app.use(express.json());
@@ -14,11 +37,4 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', sheetRoutes);
 
-// Tạo HTTP server để WebSocket có thể sử dụng chung
-const server = http.createServer(app);
-
-// Khởi động WebSocket Server
-WebSocketManager.setup(server);
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+module.exports = app;
